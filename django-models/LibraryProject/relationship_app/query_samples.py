99a4d2e2fe57cf09b39c595b7d6de04d83dd6c7a
rolandslt <lulangoroland46@gmail.com>
2025-11-08 14:50:42 +0200

    revise the query_sample file

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,19 +1,18 @@
-from relationship_app.models import Author, Book, Library
+from relationship_app.models import Author, Library
 
 # 1. Query all books by a specific author
-author_name = "J.K. Rowling"
-books_by_author = Book.objects.filter(author__name=author_name)
-print(f"Books by {author_name}:")
+author = Author.objects.get(name="J.K. Rowling")
+books_by_author = author.books.all()  # use .all() on related_name
+print(f"Books by {author.name}:")
 for book in books_by_author:
     print("-", book.title)
 
 # 2. List all books in a specific library
-library_name = "Central Library"
-books_in_library = Book.objects.filter(library__name=library_name)
-print(f"\nBooks in {library_name}:")
+library = Library.objects.get(name="Central Library")
+books_in_library = library.books.all()  # use .all() on ManyToManyField
+print(f"\nBooks in {library.name}:")
 for book in books_in_library:
     print("-", book.title)
 
-# 3. Retrieve the librarian for a specific library
-library = Library.objects.get(name=library_name)
-print(f"\nLibrarian for {library_name}: {library.librarian.name}")
+# 3. Retrieve the librarian for a library
+print(f"\nLibrarian for {library.name}: {library.librarian.name}")
