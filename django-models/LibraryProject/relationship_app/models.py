873e6ff09a6c03393399472b7183b179946216a0
rolandslt <lulangoroland46@gmail.com>
2025-11-08 14:45:16 +0200

    add book model  library modeland librarina model

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -6,3 +6,24 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Book(models.Model):
+    title = models.CharField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author')
+
+    def __str__(self):
+        return self.title
+
+class Library(models.Model):
+    name = models.CharField()
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+    
+class Librarian(models.Model):
+    name = models.CharField()
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name= 'library')
+
+    def __str__(self):
+        return self.name
