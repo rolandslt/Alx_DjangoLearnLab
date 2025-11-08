b43f8336dc9ed0fc897613f40a555e549bcf22aa
rolandslt <lulangoroland46@gmail.com>
2025-11-08 17:23:35 +0200

    add views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from .views import list_books, library_detail
- 
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('relationship_app/', list_books, name='list_books'),
-    path('relationship_app/', library_detail.as_view(), name='library_detail'),
+    path('relationship_app/', views.list_books, name='list_books'),
+    path('relationship_app/', views.library_detail.as_view(), name='library_detail'),
 ]
