0f91aa9cb80021a2db0751df83241629208cb699
rolandslt <lulangoroland46@gmail.com>
2025-11-08 22:12:51 +0200

    add admin user

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -30,3 +30,9 @@ class register(CreateView):
 
 
 
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
