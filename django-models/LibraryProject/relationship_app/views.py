bf56472b50740c6fe814b6f19591188d508d6cce
rolandslt <lulangoroland46@gmail.com>
2025-11-08 19:40:36 +0200

    add login view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,7 @@ from .models import Library
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import resolvers_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import login , logout
 
 from django.views.generic.detail import DetailView
 
