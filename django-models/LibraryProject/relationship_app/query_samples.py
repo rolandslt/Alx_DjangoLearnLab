ab7142cf1b9b9187561293d34cd7caf465d5fa29
rolandslt <lulangoroland46@gmail.com>
2025-11-08 14:55:35 +0200

    revise the query file

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,14 +1,14 @@
 from relationship_app.models import Author, Library
 
 # 1. Query all books by a specific author
-author = Author.objects.get(name="J.K. Rowling")
+author = Author.objects.get(name="author_name")
 books_by_author = author.books.all()  # use .all() on related_name
 print(f"Books by {author.name}:")
 for book in books_by_author:
     print("-", book.title)
 
 # 2. List all books in a specific library
-library = Library.objects.get(name="Central Library")
+library = Library.objects.get(name="library_name")
 books_in_library = library.books.all()  # use .all() on ManyToManyField
 print(f"\nBooks in {library.name}:")
 for book in books_in_library:
