2575eee810a9361dac0ae6018cc2a4f8ad18a7d4
rolandslt <lulangoroland46@gmail.com>
2025-11-08 20:48:44 +0200

    create admin_view file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,7 +9,7 @@ from django.urls import resolvers_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login , logout
 from django.views.generic.detail import DetailView
-from django.contrib.auth.decorators import user_passes_test
+
 def list_books(request):
     books = Book.objects.all()
     context = {'books': books}
@@ -29,23 +29,4 @@ class register(CreateView):
     template_name = 'relationship_app/register.html'
 
 
-def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
-def is_librarian(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
-
-def is_member(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
-
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
-
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'relationship_app/librarian.html')
-
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
