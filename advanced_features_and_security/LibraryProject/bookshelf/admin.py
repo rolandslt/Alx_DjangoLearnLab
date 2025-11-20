313bc79d5ef4de2e8a92b9383a6496094c8fed45
rolandslt <lulangoroland46@gmail.com>
2025-11-20 23:02:32 +0200

    add authentication with AbstractUser

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Book , CustomerUser
-
+from .models import Book , CustomUser
+from django.contrib.auth.admin import UserAdmin
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('author', 'publication_year')
     search_fields = ('title', 'author')
 
 admin.site.register(Book)
-admin.site.register(CustomerUser)
+
+class CustomUserAdmin(UserAdmin):
+    pass 
+admin.site.register(CustomUser, CustomUserAdmin)
