17a7abdf4650a02b9bf770452b669d7c11f84454
rolandslt <lulangoroland46@gmail.com>
2025-11-29 23:05:11 +0200

    secure api endpoint with  token verification  and permission

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path , include
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
-
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
@@ -13,4 +13,5 @@ urlpatterns = [
 
     # Include the router URLs for BookViewSet (all CRUD operations)
     path('', include(router.urls)),  # This includes all routes registered with the router
+    path('api-token-auth/', obtain_auth_token, name='api-token-auth'),# token endpoint
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,10 +3,17 @@ from rest_framework import generics, viewsets
 # Create your views here.
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get_permission(self):
+        if self.action in ['destroy', 'update', 'partial_update']:
+            self.permission_classes = [IsAdminUser]
+            return super().get_permissions()
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
