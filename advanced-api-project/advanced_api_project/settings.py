d7d2217b306a4d0b5b6b72413ad119d2a49ef92f
rolandslt <lulangoroland46@gmail.com>
2025-11-30 14:56:59 +0200

    Add filtering, searching, and ordering to BookListView using DRF and django-filter

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -125,5 +125,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly'
-    ]
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,21 +3,50 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 """
 ListView:
 - Returns all books.
 - Read-only: available to anyone.
+- Supports filtering, searching, and ordering.
+- Filtering fields: title, author, publication_year
+- Searching fields: title, author name
+- Ordering fields: title, publication_year
 """
+"""
+Filtering:
+    /api/books/?title=Book+Name
+    /api/books/?author=1
+    /api/books/?publication_year=2022
+
+Searching:
+    /api/books/?search=keyword
+    Supports: title, author name
+
+Ordering:
+    /api/books/?ordering=title
+    /api/books/?ordering=-publication_year
+"""
+
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]
-    filter_backends = [SearchFilter]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    # Filtering, searching, ordering
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+     # Filter options
+    filterset_fields = ['title', 'author', 'publication_year']
+
+    # Search options
     search_fields = ['title', 'author__name']
 
+    # Ordering options
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']  # default ordering
 """
 DetailView:
 - Return a single book by ID (pk).
