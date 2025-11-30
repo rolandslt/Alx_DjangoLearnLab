f7c46b2efa69be1a782d365c8f1ffb6f1f9d1857
rolandslt <lulangoroland46@gmail.com>
2025-11-30 14:58:45 +0200

    Add import from django_filters.rest_framework to satisfy filtering task requirements

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import SearchFilter, OrderingFilter
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
+
 from .models import Book
 from .serializers import BookSerializer
 """
@@ -37,7 +38,8 @@ class BookListView(generics.ListAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
     
     # Filtering, searching, ordering
-    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filter_backends = [rest_framework.DjangoFilterBackend, SearchFilter, OrderingFilter]
+
      # Filter options
     filterset_fields = ['title', 'author', 'publication_year']
 
