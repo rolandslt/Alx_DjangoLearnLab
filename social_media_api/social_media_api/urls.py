fe3d7d79837802415cc4214d1b898ca45d9dbb38
rolandslt <lulangoroland46@gmail.com>
2025-12-20 17:37:58 +0200

    config post api

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
-    path('', include('posts.urls')),
+    path('api/', include('posts.urls')), 
+    path('api/accounts/', include('accounts.urls')),
 ]
