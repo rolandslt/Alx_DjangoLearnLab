04dec4ff17d5e70fb0c67541ba449ee11db4e9bd
rolandslt <lulangoroland46@gmail.com>
2025-12-20 17:55:27 +0200

    fix accounts views

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -33,18 +33,24 @@ class LoginView(ObtainAuthToken):
 
 class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
-
+    queryset = CustomUser.objects.all()
+    
     def post(self, request, user_id):
-        user_to_follow = get_object_or_404(CustomUser, id=user_id)
+        # Explicitly using the queryset to find the user
+        user_to_follow = self.get_queryset().get(id=user_id)
+        
         if user_to_follow == request.user:
             return Response({"error": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
+        
         request.user.following.add(user_to_follow)
         return Response({"message": f"You are now following {user_to_follow.username}"}, status=status.HTTP_200_OK)
 
 class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
+    queryset = CustomUser.objects.all()
 
     def post(self, request, user_id):
-        user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+        user_to_unfollow = self.get_queryset().get(id=user_id)
+        
         request.user.following.remove(user_to_unfollow)
         return Response({"message": f"You have unfollowed {user_to_unfollow.username}"}, status=status.HTTP_200_OK)
